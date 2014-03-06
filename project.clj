f8b7ad699451ccf5bf196319aceb58df7974fdfb
John Cowie <john.a.cowie@gmail.com>
2014-03-06 17:00:26 +0530

    First commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject twang "0.1.0-SNAPSHOT"
+  :description "A library for doing twitter oauth easily with compojure"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]])
